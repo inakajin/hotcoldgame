98263f06f7693f00e77d1ab053749cab3a508efd
Mike Dickerson <inakajin@gmail.com>
2018-04-29 15:14:15 -0500

    updated testing

--- src/components/guess-count.test.js
+++ src/components/guess-count.test.js
@@ -4,15 +4,15 @@ import { shallow } from 'enzyme';
 import GuessCount from './guess-count';
 
 describe('<GuessCount />', () => {
-    it('Renders without crashing', () => {
-        shallow(<GuessCount />);
-    });
+  it('Renders without crashing', () => {
+    shallow(<GuessCount />);
+  });
 
-    it('Renders the correct guess count()', () => {
-        const value = 5;
-        const wrapper = shallow(<GuessCount GuessCount={value} />);
-        expect(wrapper.text()).toEqual(`You've made ${value} guesses!`);
-    });
+  it('Renders the correct guess count', () => {
+    const value = 5;
+    const wrapper = shallow(<GuessCount guessCount={value} />);
+    expect(wrapper.text()).toEqual(`You've made ${value} guesses!`);
+  });
 });
 //import './guess-count.css';
 
